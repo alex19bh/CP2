574cec419b89a819b46af2838412a3d91cf544c0
alex19bh <alejandro.ortiz.salduba@gmail.com>
2025-07-14 20:01:24 +0200

    Add files via upload

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = {
+    environment = "casopractico2"
+  }
+}
---
+++ terraform/vars.tf
@@ -0,0 +1,11 @@
+variable "location" {
+  default     = "westeurope"
+}
+
+variable "resource_group_name" {
+  default     = "rg-CP2alex"
+}
+
+variable "acr_name" {
+  default     = "acrCP2alex"
+}
